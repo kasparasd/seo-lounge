8224bdfce052cbeb527f1edd7afc97bd67f96584
Kasparas <kasparas.dapsys@gmail.com>
2023-11-20 10:05:56 +0200

    content

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Services } from './components/Services';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <Services/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
